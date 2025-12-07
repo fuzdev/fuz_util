f2fbd39aef3d237ee170ef243b48c3f378442805
Ryan Atkinson <mail@ryanatkn.com>
2025-12-07 11:22:45 -0500

    wip

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -21,9 +21,9 @@ export type OmitStrict<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
  * @see https://stackoverflow.com/users/5770132/oblosys
  */
 export type PickUnion<T, K extends KeyofUnion<T>> = T extends unknown
-	? K & keyof T extends never // eslint-disable-line @typescript-eslint/no-redundant-type-constituents
+	? K & keyof T extends never
 		? never
-		: Pick<T, K & keyof T> // eslint-disable-line @typescript-eslint/no-redundant-type-constituents
+		: Pick<T, K & keyof T>
 	: never;
 
 /**
