1ece2821cf851db04b7e1e4277557f2e6c2953a7
Ryan Atkinson <mail@ryanatkn.com>
2026-01-03 08:00:00 -0500

    wip

--- src/routes/library.gen.ts
+++ src/routes/library.gen.ts
@@ -1,4 +1,5 @@
 import {library_gen} from '@fuzdev/fuz_ui/library_gen.js';
-import {library_throw_on_duplicates} from '@fuzdev/fuz_ui/library_generate.js';
+// TODO BLOCK move to fuz_ui/library_gen.ts?
+import {library_throw_on_duplicates} from '@fuzdev/svelte-docinfo';
 
 export const gen = library_gen({on_duplicates: library_throw_on_duplicates});
--- src/routes/library_json.ts
+++
@@ -1,2 +0,0 @@
-// TODO @many maybe rework src/routes/library.gen.ts so this is unnecessary
-export * from '$lib/library_json.js';
