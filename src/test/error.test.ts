bdc96d81ddfc6cfb5af149dfe9b6d131a59ab6fc
Ryan Atkinson <mail@ryanatkn.com>
2026-05-29 19:35:02 -0400

    wip

--- src/test/error.test.ts
+++ src/test/error.test.ts
@@ -42,7 +42,7 @@ test('unreachable helper with custom message', () => {
 	}
 
 	assert.ok(caught_error instanceof UnreachableError);
-	assert.strictEqual((caught_error as UnreachableError).message, custom_message);
+	assert.strictEqual((caught_error).message, custom_message);
 });
 
 test('unreachable helper requires never type parameter', () => {
