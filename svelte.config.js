85662504b5a1f42f88b9321fb5697fca75e69a16
Ryan Atkinson <mail@ryanatkn.com>
2026-05-29 15:01:24 -0400

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,7 @@ import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static';
 import {svelte_preprocess_mdz} from '@fuzdev/fuz_ui/svelte_preprocess_mdz.js';
 import {svelte_preprocess_fuz_code} from '@fuzdev/fuz_code/svelte_preprocess_fuz_code.js';
-import {create_csp_directives} from '@fuzdev/fuz_ui/csp.js';
-import {csp_trusted_sources_of_fuzdev} from '@fuzdev/fuz_ui/csp_of_fuzdev.js';
+import {csp_directives_of_fuzdev} from '@fuzdev/fuz_ui/csp_of_fuzdev.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -17,10 +16,6 @@ export default {
 			$routes: 'src/routes',
 			'@fuzdev/fuz_util': 'src/lib',
 		},
-		csp: {
-			directives: create_csp_directives({
-				trusted_sources: csp_trusted_sources_of_fuzdev,
-			}),
-		},
+		csp: {directives: csp_directives_of_fuzdev},
 	},
 };
